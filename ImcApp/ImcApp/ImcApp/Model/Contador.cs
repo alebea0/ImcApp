3a2f96031e1c5153ee7187e207be9aeffc095eae
Alejandra B. C. Glez <86536719+alebea0@users.noreply.github.com>
2021-10-28 16:50:55 -0500

    Agrega la clase Contador y realiza el conteo utilizando una instancia  de esta clase.

---
+++ ImcApp/ImcApp/ImcApp/Model/Contador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImcApp.Model
+{
+    public class Contador
+    {
+        private int _Conteo;
+        public int Conteo
+        {
+            get => _Conteo;
+            set
+            {
+                if (_Conteo != value)
+                {
+                    _Conteo = value;
+                }
+            }
+        }
+        public Contador()
+        {
+            ReiniciarConteo();
+        }
+        public void Contar()
+        {
+            Conteo++;
+        }
+        public void ReiniciarConteo()
+        {
+            Conteo = 0;
+        }
+    }
+}
