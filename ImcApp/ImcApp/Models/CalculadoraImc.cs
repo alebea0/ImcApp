8278c777a7d725aaf722928b9475a8312ef4cc5c
Alejandra B. C. Glez <86536719+alebea0@users.noreply.github.com>
2022-01-08 14:51:55 -0600

    Final

---
+++ ImcApp/ImcApp/Models/CalculadoraImc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImcApp.Models
+{
+    public class CalculadoraImc
+    {
+        public static decimal IndiceDeMasaCorporal(decimal peso, decimal estatura)
+            => peso / (estatura * estatura);
+
+        public enum EstadoNutricional
+        {
+            PesoBajo,
+            PesoNormal,
+            SobrePeso,
+            Obesidad,
+            ObesidadExtrema
+        }
+        public static EstadoNutricional SituacionNutricional(decimal imc)
+        {
+            if (imc < 18.5m)
+            {
+                return EstadoNutricional.PesoBajo;
+            }
+            else if (imc < 25.0m)
+            {
+                return EstadoNutricional.PesoNormal;
+            }
+            else if (imc < 30.0m)
+            {
+                return EstadoNutricional.SobrePeso;
+            }
+            else if (imc < 40.0m)
+            {
+                return EstadoNutricional.Obesidad;
+            }
+            else
+            {
+            return EstadoNutricional.ObesidadExtrema;
+            }
+        }
+    }
+}
---
+++ ImcApp/ImcApp/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using ImcApp.Models;
+using ImcApp.ViewModels;
+
+namespace ImcApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel viewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new MainPageViewModel();
+            BindingContext = viewModel;
+        }
+    }
+}
